a5cb8d4dadf267a7360fa4bbcc69ad2617abb18e
creatorclark <digitalcraftsca@gmail.com>
2021-06-22 19:16:57 -0400

    update files

---
+++ js/script.js
@@ -0,0 +1,54 @@
+//poll
+        /*const btn = document.querySelector('#btn');
+        // handle click button
+        btn.onclick = function () {
+            const rbs = document.querySelectorAll('input[name="choice"]');
+            let selectedValue;
+            for (const rb of rbs) {
+                if (rb.checked) {
+                    selectedValue = rb.value;
+                    break;
+                }
+            }
+            alert(selectedValue);
+        };*/
+
+/*document.getElementsByName("Food").addEventListener("click", pollFunction);
+function pollFunction() {
+    document.getElementById("btn").innerHTML= "is this what i wanted";
+}*/
+/*let container = document.querySelector("#input-container")
+let saying = container.querySelector("h2"); /*trying to replace h2 "Based on the videos above, which space food would you most likely consume:"
+
+saying.forEach(function(match) {
+    match.parentNode.removeChild(match);
+});*/
+
+/*var radios = document.forms["form"].elements["input"];
+  for(radio in radios) {
+    btn.onclick = myfunction(); {
+        document.getElementByID(h2).innerhtml=(radios.value);
+    }
+}*/
+
+/*const button = document.querySelector('#btn')
+  
+  button.onclick = function() {
+      for 
+    alert('clicked')
+  }
+
+  $('#foodRadio').click(function() {     
+    var checked = $(this).attr('checked', true);
+    if(checked){ 
+      $(this).attr('checked');
+      return
+    }
+    else{ 
+      $(this).attr('checked', fals);
+    }
+  });
+
+  $('#myForm input').on('change', function() {
+    alert($('input[name=food1]:checked', '#myForm').val()); 
+ });*/
